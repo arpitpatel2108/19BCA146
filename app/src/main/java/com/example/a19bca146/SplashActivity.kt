9d693bbcd2a075240f705d23f7de1db860ac31c5
ARPIT <19bca159@charusat.edu.in>
2022-04-25 09:55:50 +0530

    19BCA146

---
+++ app/src/main/java/com/example/a19bca146/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.a19bca146
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        var image=imageView
+        var fadeIn= AnimationUtils.loadAnimation(applicationContext,R.anim.fade_in)
+        // fadeIn.setAnimationListener(this);
+        image.startAnimation(fadeIn);
+
+
+        Handler().postDelayed(Runnable {
+
+
+            var intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        },8000)
+    }
+}
